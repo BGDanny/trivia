d99fc27860f42473a8f1d1fbd014441bfe4ed55c
BGDanny <dannyxue121@gmail.com>
2023-03-01 13:13:43 -0700

    Add homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,30 @@
-import * as React from "react"
+import * as React from "react";
 import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+    ChakraProvider,
+    VStack,
+    theme,
+    Heading,
+    Button,
+    Input,
+} from "@chakra-ui/react";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+export const App = () => {
+    const [code, setCode] = React.useState("");
+    return (
+        <ChakraProvider theme={theme}>
+            <VStack textAlign="center" fontSize="xl">
+                <Heading>Trivia</Heading>
+                <Button>
+                    {code ? "Join Lobby with Code" : "Create Lobby"}
+                </Button>
+                <Input
+                    placeholder="Lobby Code"
+                    value={code}
+                    onChange={(e) => setCode(e.target.value)}
+                    type="text"
+                    w={"min"}
+                />
+            </VStack>
+        </ChakraProvider>
+    );
+};
