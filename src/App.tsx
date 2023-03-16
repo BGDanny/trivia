d8b433f254e5677ab07881c5b30c21ecf0a00edf
BGDanny <dannyxue121@gmail.com>
2023-03-16 17:48:30 -0600

    Add websocket

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import * as React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const Home = React.lazy(() => import("./pages/Home"));
 const Lobby = React.lazy(() => import("./pages/Lobby"));
 const Game = React.lazy(() => import("./pages/Game"));
 const Result = React.lazy(() => import("./pages/Result"));
+
+const socket = new WebSocket("ws://68.146.50.113:8000/ws");
+
+socket.addEventListener("message", (e) => console.log(e.data));
 
 export const App = () => {
     const queryClient = new QueryClient({
@@ -18,12 +22,13 @@ export const App = () => {
     });
 
     const router = createBrowserRouter([
-        { path: "/", element: <Home /> },
+        { path: "/", element: <Home socket={socket} /> },
         { path: "/lobby", element: <Lobby /> },
         { path: "/game", element: <Game /> },
         { path: "/result", element: <Result /> },
         { path: "*", element: <h1>Not Available</h1> },
     ]);
+
     return (
         <QueryClientProvider client={queryClient}>
             <ChakraProvider theme={theme}>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,50 +1,144 @@
-import * as React from "react";
+import { CheckIcon } from "@chakra-ui/icons";
 import {
-    VStack,
-    Heading,
-    Button,
-    Input,
     Box,
-    SimpleGrid,
+    Button,
     Card,
-    CardBody,
     CardFooter,
+    CardHeader,
+    Heading,
+    IconButton,
+    Input,
+    InputGroup,
+    InputRightElement,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    SimpleGrid,
+    Tooltip,
+    VStack,
 } from "@chakra-ui/react";
+import * as React from "react";
 import BackgroundImage from "../../assets/pexels.jpg";
 
-export const Home: React.FC = () => {
+export const Home: React.FC<{ socket: WebSocket }> = ({ socket }) => {
+    const [isOpen, setOpen] = React.useState(false);
+    const [code, setCode] = React.useState("");
+    const [username, setUsername] = React.useState("");
     return (
-        <VStack>
-            <Box
-                backgroundImage={BackgroundImage}
-                bgRepeat="no-repeat"
-                backgroundSize="cover"
-                textAlign={"center"}
-                width="100%"
-            >
-                <Heading
-                    fontSize="40"
-                    lineHeight={"tall"}
-                    bgGradient="linear(to-r, teal.400, blue.700)"
-                    bgClip={"text"}
+        <Box>
+            <VStack spacing={20} textAlign={"center"}>
+                <Box
+                    backgroundImage={BackgroundImage}
+                    bgRepeat="no-repeat"
+                    backgroundSize="cover"
+                    width="100%"
                 >
-                    Trivia Battleground
-                </Heading>
-            </Box>
-            <SimpleGrid columns={2} mt={500}>
-                <Card bgColor={"#E4DCCF"}>
-                    <CardBody>Join a lobby using an access code</CardBody>
-                    <CardFooter>
-                        <Input placeholder="Lobby Code" type="text" size="md" variant={"filled"}/>
-                    </CardFooter>
-                </Card>
-                <Card bgColor="#EA5455">
-                    <CardBody>Create your own lobby</CardBody>
-                    <CardFooter>
-                        <Button >Create Lobby</Button>
-                    </CardFooter>
-                </Card>
-            </SimpleGrid>
-        </VStack>
+                    <Heading
+                        fontSize="60"
+                        lineHeight={"tall"}
+                        bgGradient="linear(to-r, teal.400, blue.800)"
+                        bgClip={"text"}
+                    >
+                        Trivia Battleground
+                    </Heading>
+                </Box>
+                <SimpleGrid columns={2} spacing={5}>
+                    <Card bgColor={"#E4DCCF"}>
+                        <CardHeader as="b" fontSize={40}>
+                            Join a Lobby
+                        </CardHeader>
+                        <CardFooter>
+                            <InputGroup>
+                                <Input
+                                    placeholder="Access Code"
+                                    type="text"
+                                    size="md"
+                                    variant={"filled"}
+                                    value={code}
+                                    onChange={(e) => setCode(e.target.value)}
+                                />
+                                {code && (
+                                    <Tooltip label="Submit">
+                                        <InputRightElement
+                                            children={
+                                                <IconButton
+                                                    icon={<CheckIcon />}
+                                                    aria-label="Submit"
+                                                    colorScheme={"green"}
+                                                    variant="link"
+                                                    onClick={() =>
+                                                        setOpen(true)
+                                                    }
+                                                />
+                                            }
+                                        />
+                                    </Tooltip>
+                                )}
+                            </InputGroup>
+                        </CardFooter>
+                    </Card>
+                    <Card bgColor="#EA5455">
+                        <CardHeader as="b" fontSize={40}>
+                            Create a New Lobby
+                        </CardHeader>
+                        <CardFooter>
+                            <Button
+                                width="100%"
+                                onClick={() => {
+                                    setOpen(true);
+                                    setCode("");
+                                }}
+                            >
+                                Create Lobby
+                            </Button>
+                        </CardFooter>
+                    </Card>
+                </SimpleGrid>
+            </VStack>
+            <Modal
+                isOpen={isOpen}
+                onClose={() => {
+                    setOpen(false);
+                    setUsername("");
+                }}
+                isCentered
+            >
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Enter Your Username</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Input
+                            placeholder="Username"
+                            type="text"
+                            size="md"
+                            variant={"filled"}
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button
+                            colorScheme="teal"
+                            onClick={() => {
+                                if (code) {
+                                    socket.send(
+                                        `Join Room:${username}:${code}`
+                                    );
+                                } else {
+                                    socket.send("Create Room:" + username);
+                                }
+                            }}
+                        >
+                            Confirm
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </Box>
     );
 };
