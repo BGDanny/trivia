5aafe1ba979a98bde55ad17cd8db7d6d6a1d05d4
BGDanny <dannyxue121@gmail.com>
2023-03-15 22:11:15 -0600

    Rework homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,50 @@
 import * as React from "react";
-import { VStack, Heading, Button, Input } from "@chakra-ui/react";
+import {
+    VStack,
+    Heading,
+    Button,
+    Input,
+    Box,
+    SimpleGrid,
+    Card,
+    CardBody,
+    CardFooter,
+} from "@chakra-ui/react";
+import BackgroundImage from "../../assets/pexels.jpg";
 
 export const Home: React.FC = () => {
-    const [code, setCode] = React.useState("");
     return (
-        <VStack textAlign="center" fontSize="xl">
-            <Heading>Trivia</Heading>
-            <Button>{code ? "Join Lobby with Code" : "Create Lobby"}</Button>
-            <Input
-                placeholder="Lobby Code"
-                value={code}
-                onChange={(e) => setCode(e.target.value)}
-                type="text"
-                w={"min"}
-            />
+        <VStack>
+            <Box
+                backgroundImage={BackgroundImage}
+                bgRepeat="no-repeat"
+                backgroundSize="cover"
+                textAlign={"center"}
+                width="100%"
+            >
+                <Heading
+                    fontSize="40"
+                    lineHeight={"tall"}
+                    bgGradient="linear(to-r, teal.400, blue.700)"
+                    bgClip={"text"}
+                >
+                    Trivia Battleground
+                </Heading>
+            </Box>
+            <SimpleGrid columns={2} mt={500}>
+                <Card bgColor={"#E4DCCF"}>
+                    <CardBody>Join a lobby using an access code</CardBody>
+                    <CardFooter>
+                        <Input placeholder="Lobby Code" type="text" size="md" variant={"filled"}/>
+                    </CardFooter>
+                </Card>
+                <Card bgColor="#EA5455">
+                    <CardBody>Create your own lobby</CardBody>
+                    <CardFooter>
+                        <Button >Create Lobby</Button>
+                    </CardFooter>
+                </Card>
+            </SimpleGrid>
         </VStack>
     );
 };
